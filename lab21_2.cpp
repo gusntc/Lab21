38698cd9992d4d9ebce08c1e74998a239023ebb3
gusntc <Natacha_rungbanpant@cmu.ac.th>
2022-02-01 00:58:49 +0700

    Lab21_2Edit

--- lab21_2.cpp
+++ lab21_2.cpp
@@ -5,8 +5,24 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1,Rect *R2){
+	Rect newR1,newR2;
+	newR1.w = R1->x+R1->w;
+	newR1.h = R1->y-R1->h;
+	newR2.w = R2->x+R2->w;
+	newR2.h = R2->y-R2->h;
+	double xdis = min(newR1.w,newR2.w)-max(R1->x,R2->x);
+	double ydis = min(R1->y,R2->y)-max(newR1.h,newR2.h);
+	double area = 0;
+	if(xdis > 0 && ydis > 0){
+		double area = xdis*ydis;
+		return area;
+	}
+	return area;
+}
 
-
-
+int main(){
+	Rect R1 = {1,1,5,5};
+	Rect R2 = {7,2,3,3};	
+	cout << overlap(&R1,&R2);		
 }
